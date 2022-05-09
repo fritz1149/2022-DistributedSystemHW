412daceb2861c0f723527c7f35459500ff354b89
huahuaxiaomuzhu <56588699+huahuaxiaomuzhu@users.noreply.github.com>
2022-05-09 14:27:21 +0800

    增加广播模式发消息

--- ENS/publisher/src/main/java/com/ds/publisher/shell/SendMessageCMD.java
+++ ENS/publisher/src/main/java/com/ds/publisher/shell/SendMessageCMD.java
@@ -1,10 +1,16 @@
 package com.ds.publisher.shell;
 
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+
+import java.nio.charset.StandardCharsets;
 
 @ShellComponent
 public class SendMessageCMD {
@@ -13,13 +19,24 @@ public class SendMessageCMD {
     @ShellMethod("send many message concurrently")
     public String send(
             @ShellOption int num
-    ) {
+    ) throws MQBrokerException, RemotingException, InterruptedException, MQClientException {
         long startTime=System.currentTimeMillis();
+        Message broad=new Message("broad","broadcasting","broadcasting-message-body".getBytes(StandardCharsets.UTF_8));
+        rocketMQTemplate.getProducer().send(broad);
         Runnable sender= () -> rocketMQTemplate.convertAndSend("atopic","hello");
         for (int i=0;i<num;i++){
             new Thread(sender).start();
         }
         long endTime=System.currentTimeMillis();
         return "send "+num +" messages cost "+(endTime-startTime)+"ms";
+    }
+    @ShellMethod("send a broadcast message")
+    public String broadcast(
+            @ShellOption(defaultValue = "broad") String topic,
+            @ShellOption(defaultValue = "broadcasting-message-body")String content
+    ) throws MQBrokerException, RemotingException, InterruptedException, MQClientException {
+        Message broad=new Message(topic,"broadcasting",content.getBytes(StandardCharsets.UTF_8));
+        rocketMQTemplate.getProducer().send(broad);
+        return "broad success";
     }
 }
